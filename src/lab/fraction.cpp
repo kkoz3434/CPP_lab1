ac66d0707a602dd3d4bcab97b32a91213e7b8368
kkoz3434 <jkozlak@student.agh.edu.pl>
2021-03-16 02:50:18 +0100

    "dokoncz save load"

--- src/lab/fraction.cpp
+++ src/lab/fraction.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 #include "fraction.h"
 
+int Fraction::removedFractions_ = 0;
+
 
 #ifndef _MSC_FULL_VER // if not Visual Studio Compiler
-    #warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
+#warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
 #else
-    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
 #endif
--- src/lab/fraction.h
+++ src/lab/fraction.h
@@ -14,8 +14,16 @@ Zaimplementuj podaną na zajęciach klasę reprezentującą Ulamek (ang. Fractio
         które umożliwią modyfikacje i pobranie wartości licznika i mianownika
     metodę print(), wypisującą atrybuty obiektu na konsolę w formie licznik/mianownik\n
 
-    zadany atrybut (removedFractions_) i metodę statyczną: removedFractions().  na staticu i destruktor zmienic ~klasa
+    zadany atrybut (removedFractions_) i metodę statyczną: removedFractions().
         Skladowa ta powinna byc incrementowana w destruktorze
+
+     Proszę o utworzenie 3 stałych w klasie i metod do odczytu (tzn. getterów):
+            nazwa ułamka jako string
+                metoda: getFractionName()
+            stała statyczna odzwierciedlajaca niedopuszczalna wartość mianownika: (ang. invalidDenominatorValue)
+                metoda: getInvalidDenominatorValue()
+            statyczna stała czasu kompilacji (constexpr) odzwierciedlajaca domyślna wartość mianownika: (ang. defaultDenominatorValue)
+                metoda: getDefaultDenominatorValue()
 
 
 
@@ -27,46 +35,52 @@ Zaimplementuj podaną na zajęciach klasę reprezentującą Ulamek (ang. Fractio
 
 
 
-    Proszę o utworzenie 3 stałych w klasie i metod do odczytu (tzn. getterów):
-        nazwa ułamka jako string
-            metoda: getFractionName()
-        stała statyczna odzwierciedlajaca niedopuszczalna wartość mianownika: (ang. invalidDenominatorValue)
-            metoda: getInvalidDenominatorValue()
-        statyczna stała czasu kompilacji (constexpr) odzwierciedlajaca domyślna wartość mianownika: (ang. defaultDenominatorValue)
-            metoda: getDefaultDenominatorValue()
+
+
 **/
 
-constexpr bool classFractionExists = false;
-constexpr bool hasNumeratorAndDenominator = false;
-constexpr bool hasConstructorWhichInitialiseFields = false;
-constexpr bool hasGettersAndSetters = false;
+constexpr bool classFractionExists = true;
+constexpr bool hasNumeratorAndDenominator = true;
+constexpr bool hasConstructorWhichInitialiseFields = true;
+constexpr bool hasGettersAndSetters = true;
 constexpr bool hasPrintFunction = true;
-constexpr bool counterOfDestructedFractionsImplemented = false;
+constexpr bool counterOfDestructedFractionsImplemented = true;
 constexpr bool readWriteImplemented = false;
-constexpr bool fractionNameSettableFromConstructor = false;
-constexpr bool fractionConstStaticFieldsImplemented = false;
+constexpr bool fractionNameSettableFromConstructor = true;
+constexpr bool fractionConstStaticFieldsImplemented = true;
 
 class Fraction {
 protected:
     int numerator;
     int denominator;
+    static int removedFractions_;
+    std::string fractionName;
+
+    static constexpr int invalidDenominatorValue {0};
+    static constexpr int defaultDenominatorValue {1};
+
 
 public:
 
-    static constexpr int removedFractions_ = 0;
-
     // constructors
-    Fraction() {
-        numerator = 0;
-        denominator = 1;
-    }
+    Fraction(): numerator {0},denominator {1} {}
 
-    Fraction(int num, int denom) {
-        numerator = num;
-        denominator = denom;
+    Fraction(int num, int denom): numerator {num},denominator {denom} {}
+
+    Fraction(int num, int denom, std::string name): numerator {num},denominator {denom}, fractionName {name} {}
+
+    ~Fraction(){
+        removedFractions_++;
     }
 
     // setters and getters
+    static int getInvalidDenominatorValue(){
+        return invalidDenominatorValue;
+    }
+    static int getDefaultDenominatorValue(){
+        return defaultDenominatorValue;
+    }
+
     int getNumerator() {
         return numerator;
     }
@@ -83,12 +97,31 @@ public:
         denominator = s;
     }
 
-    void print(){
-        std:: cout<< numerator/denominator << std::endl;
+    void print() const {
+        std::string result = std::to_string(numerator) + "/" + std::to_string(denominator);
+        std::cout << result;
     }
-    static int removedFractions(){
+
+    static int removedFractions() {
         return removedFractions_;
     }
+
+    void save(ostream& os){
+        int a,b;
+        os << a;
+        os << b;
+        numerator = a;
+        denominator = b;
+    }
+
+    void load(istream& is){
+
+    }
+
+    std::string getFractionName(){
+        return fractionName;
+    }
+
 
 };
 
